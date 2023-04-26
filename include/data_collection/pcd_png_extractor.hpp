1871ab4388a618ac1d9e5ef0f3462d3ad4f51d41
zymouse <mouse_zy@qq.com>
2023-04-26 17:04:32 +0800

    feat:新增pcd和png采集

---
+++ include/data_collection/pcd_png_extractor.hpp
@@ -0,0 +1,11 @@
+#ifndef __DATA_COLLECTION__PCD_PNG_EXTRACTOR__HPP__
+#define __DATA_COLLECTION__PCD_PNG_EXTRACTOR__HPP__
+
+// ros2
+#include <rclcpp/rclcpp.hpp>
+
+// msgs
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs>
+
+#endif // __DATA_COLLECTION__PCD_PNG_EXTRACTOR__HPP__
